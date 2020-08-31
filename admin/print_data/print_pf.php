c7cd2894061e04f8678280db287af874ab6d85a6
andrean <andrean@gmail.com>
2020-08-31 16:56:03 +0700

    Tambah Data

--- admin/print_data/print_pf.php
+++ admin/print_data/print_pf.php
@@ -1,4 +1,4 @@
-
+<?php include 'config/koneksi.php'; ?>
 <h2 align="center"><img src="img/icon.jpg" width="100" height="100" style="margin-left: 30px;">Aprtement Bukit Golf Pondok indah</h2>
 <h3 align="center">Data PREVENTIF MAINTENANCE PREASURE FAN</h3>
 <div class="card-body">
